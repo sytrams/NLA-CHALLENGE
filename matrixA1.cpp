84dcd840390fa4c183f5ed714ed567fa88a00688
anneaaaa <annea.patricia15@gmail.com>
2025-09-26 11:55:34 +0200

    Update matrixA1.cpp

--- matrixA1.cpp
+++ matrixA1.cpp
@@ -60,3 +60,49 @@ SparseMatrix<double> A1(width*height, width*height);
     cout << "Number of non-zeros in A1: " << A1.nonZeros() << endl;
 
     smooth = A1 * w; //smoothing operation
+
+    //add control for values of smooth matrix
+
+     for (int i = 0; i < height; ++i) {
+      for (int j = 0; j < width; ++j) {
+        if(smooth(i,j)<0){
+          smooth(i,j)=0.0;}
+        if(smooth(i,j)>1){
+          smooth(i,j)=1.0;}
+      }
+    }
+    ....
+
+//now sharpening
+     std::vector<T> tripletList2;
+    tripletList2.reserve(width*height);
+    
+      // fill the triplet list with the values for the diagonal matrix
+      for( int r =0; r<height; r++){
+        for(int c=0; c< width; c++){
+          //r*width+c is the index of the pixel in the vectorized form
+          tripletList2.push_back(T(r*width+c, r*width+c, 9.0)); //diagonal element
+          if(c>0) //left neighbor
+            tripletList2.push_back(T(r*width+c, r*width+c-1, -2.0));
+          if(c<width-1) //right neighbor
+            tripletList2.push_back(T(r*width+c, r*width+c+1,-2.0));
+          if(r>0) //top neighbor
+            tripletList2.push_back(T(width*(r-1)+c, width*r+c, -2.0));
+          if(r<height-1) //bottom neighbor
+            tripletList2.push_back(T(width*(r+1)+c, width*r+c, -2.0));
+        }
+      }
+    
+    A2.setFromTriplets(tripletList2.begin(), tripletList2.end());
+
+    sharp = A2 * v; //sharping operation
+
+
+     for (int i = 0; i < height; ++i) {
+      for (int j = 0; j < width; ++j) {
+        if(sharp(i,j)<0){
+          sharp(i,j)=0.0;}
+        if(sharp(i,j)>1){
+          sharp(i,j)=1.0;}
+      }
+    }
