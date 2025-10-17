af4acef30485926579f8f9b06f9bbf524745a038
Annea Patricia Marienne Reyes <annea.patricia15@gmail.com>
2025-10-17 23:26:54 +0200

    Update challenge2.cpp

--- challenge2.cpp
+++ challenge2.cpp
@@ -2,6 +2,7 @@
 #include <Eigen/Sparse>
 #include <Eigen/Dense>
 #include <cmath>
+#include <limits>
 
 using namespace std;
 using namespace Eigen;
@@ -31,8 +32,65 @@ int main(int argc, char** argv)
 
     //Frobenius norm, Ag is symmetric
     double fb = sqrt((Ag*Ag).trace());
-    cout<< "Frobenius norm: "<< fb <<endl;
+    cout<< "Frobenius norm of Ag: "<< fb <<endl;
 
+    VectorXd vg= Ag.rowwise().sum();
+    //cout<< "vg: "<<"\n" <<vg <<endl;
+
+    //define Dg diagonal matrix with vg as diagonal
+    MatrixXd Dg = vg.asDiagonal();
+    //cout<< "Dg: "<<"\n" <<Dg <<endl;
+
+    MatrixXd Lg = Dg -Ag;
+    VectorXd x = VectorXd::Ones(n);
+
+    // y=Lg*x
+    VectorXd y = Lg*x;
+    cout<<"Euclidian norm of y: " <<y.norm()<<endl;
+
+    //verify if Lg is SPD
+    if (Lg.isApprox(Lg.transpose())) {
+        cout << "Lg is symmetric." << endl;
+        //control if definite postiive
+        LLT<MatrixXd> llt;
+        llt.compute(Lg);
+        if(llt.info()!= Success) {
+            cerr << "Lg isn't positive definite." << endl;
+        }else {
+            cout << "Lg is positive definite." << endl;
+        }
+    } else {
+        cout << "Lg is not symmetric." << endl;
+    }
+
+    SelfAdjointEigenSolver<MatrixXd> eigensolver(Lg); //only computes real eigen values (and Lg is real matrix)
+    if (eigensolver.info() != Eigen::Success) abort();
+    //cout << "The eigenvalues of Lg:\n" << eigensolver.eigenvalues() << endl;
+
+    VectorXd Lg_eigenvalues = eigensolver.eigenvalues();
+    MatrixXd Lg_eigenvectors= eigensolver.eigenvectors();
+
+    //Lg is a real matrix
+    cout << "Lg's largest eigenvalue: " << Lg_eigenvalues.maxCoeff() << endl;
+    cout << "Lg's smallest eigenvalue: " << Lg_eigenvalues.minCoeff() << endl;
+
+    int smallestPositiveIndex=-1;
+    double smallestPositive = numeric_limits<double>::infinity(); //smallestPositive is infinite
+    for(int i=0; i<Lg_eigenvalues.size(); i++){
+        if (Lg_eigenvalues(i)>0 && Lg_eigenvalues(i)<smallestPositive){
+            smallestPositiveIndex=i;
+            smallestPositive = Lg_eigenvalues(i);
+        }
+    }
+
+    if (smallestPositiveIndex == -1) {
+        cout << "No positive eigenvalues found." << endl;
+    } else {
+        cout << "Smallest positive eigenvalue: " << smallestPositive << endl;
+        // Corresponding eigenvector: (eigvalue in i position -> eigvector in i column)
+        cout << "Corresponding eigenvector:\n" <<  Lg_eigenvectors.col(smallestPositiveIndex) << endl;
+    }
 
     return 0;    
 }
+
